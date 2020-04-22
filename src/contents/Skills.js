cf982fb5398ec6c54a6c7bbaf34a914abb14e649
davidmcgregor <davidjohnmcgregor@gmail.com>
2020-04-22 22:26:14 +0100

    Update myskills object and styling

--- src/contents/Skills.js
+++ src/contents/Skills.js
@@ -5,7 +5,12 @@ class Skills extends Component {
   {
       super(props);
       this.state={
-          'myskills':["JavaScript", "ReactJS", "Node.js", "APIs", "Jasmine", "Ruby", "Rails", "Sinatra", "RSpec", "Capybara", "PostgreSQL", "HTML/CSS", "TravisCI", "Heroku"]
+          'myskills':{ 
+            JavaScript: ["JavaScript", "jQuery", "ReactJS", "Node.js", "Jasmine", "Jest", "Enzyme", "ESLint", "Mocha", "Chai"],
+            Ruby: ["Ruby", "Rails", "RSpec", "Sinatra", "Capybara", "Rubocop"],
+            TechSkills: ["SQL", "APIs", "AWS", "Git", "TravisCI", "CodeClimate", "Heroku", "Netlify", "HTML", "CSS", "Bootstrap"],
+            OtherSkills: ["Agile methodology", "Test-driven development", "Process diagramming"]
+         }
       };
   }
 
@@ -13,9 +18,34 @@ class Skills extends Component {
     return (
         <div className="condiv skills">
         <h1 className="subtopic">Skills</h1>
+        <h3>JavaScript</h3>
         <ul>
-        {this.state.myskills.map((value)=>{
-            return <li className="animated slideInLeft">{value}</li>
+        {this.state.myskills.JavaScript.map((key)=>{
+            return <li className="animated slideInLeft skills">{key}</li>
+        })}
+        </ul>
+
+        <br />
+        <h3>Ruby</h3>
+        <ul>
+        {this.state.myskills.Ruby.map((key)=>{
+            return <li className="animated slideInLeft skills">{key}</li>
+        })}
+        </ul>
+
+        <br />
+        <h3>Tech Skills</h3>
+        <ul>
+        {this.state.myskills.TechSkills.map((key)=>{
+            return <li className="animated slideInLeft skills">{key}</li>
+        })}
+        </ul>
+
+        <br />
+        <h3>Other</h3>
+        <ul>
+        {this.state.myskills.OtherSkills.map((key)=>{
+            return <li className="animated slideInLeft skills">{key}</li>
         })}
         </ul>
         </div>
